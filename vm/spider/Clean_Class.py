6c35095dad21e3a63fdc7b23f04f6cc2920eff66
Jordandiazfr <jordandiazfr@gmail.com>
2021-04-09 18:24:34 +0200

    read and clean a bit the dataframe

--- vm/spider/Clean_Class.py
+++ vm/spider/Clean_Class.py
@@ -12,7 +12,7 @@ class Cleaner():
     def convert_to_csv(self, src):
         # Get the path of the current excel file 
         file_path = os.path.join(self.media_dir,src)
-        print(file_name)
+        print(file_path)
         
         # Get the name of the current excel file without extension
         file_name = src.split(".")
@@ -21,20 +21,30 @@ class Cleaner():
         cast_in_csv = file_name[0] + ".csv"
         
         # Create a new imaginary path to the csv file "that doesn't exist yet "
-        new_full_path = os.path.join(self.media_dir, o)
+        new_full_path = os.path.join(self.media_dir, cast_in_csv)
         
         # Create a new .csv file with the name that we caste
         with open(new_full_path ,mode="w") as f:
             f.write("")
             
         # Load the excel 
-        df = pd.read_excel(file_name)
-
+        df = pd.read_excel(file_path, usecols=[0,2,4,7,8,9])
+        df['Cost'] = df['Cost'].apply(lambda x: float(x.replace(',', '.')))
+        df['Quantity'] = df['Quantity'].apply(lambda x: float(x.replace(',', '.')))
+        print(df)
+        
         # Rewrite the empty .csv file with the loaded dataframe 
         df.to_csv(new_full_path, index = False)
             
-            
+    def read_csv(self, src):
+        file_path = os.path.join(self.media_dir,src)
+        df = pd.read_csv(file_path)  
+        print(df.head())       
 
 if __name__ == "__main__":
     c = Cleaner()
-    c.convert_to_csv("testfilejojo.xlsx")
+    name = "testfilejojo"
+    c.convert_to_csv("testfilejojo" + ".xlsx")
+    #c.read_csv(name  + ".csv")
+    
+    
