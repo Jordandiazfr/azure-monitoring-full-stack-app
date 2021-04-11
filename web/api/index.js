d0b4978a647d5b6144c21a60e3f99fff2af326f9
Adlane <50279061+aaadlane@users.noreply.github.com>
2021-04-11 18:06:38 +0200

    befor pull master

--- web/api/index.js
+++ web/api/index.js
@@ -1,11 +1,33 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 4000
+const db = require('./pool')
 
-app.get("/", (req,res) => {
-    res.send("Test up")
-})
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+
+app.get('/servicename', db.getServiceName)
+
+app.get('/servicename/:id', db.getServiceById)
+console.log(db.getServiceById);
+
+app.post('/servicename', db.createService)
+app.put('/servicename/:id', db.updateService)
+app.delete('/servicename/:id', db.deleteService)
+
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
 
 app.listen(PORT, (req,res) => {
     console.log(`Server up and running in port: ${PORT}`)
 })
+
